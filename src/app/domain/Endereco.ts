c7db27d7d92c3762f060586dbbdc9fc3bd0d346c
Vitor Rubim <Vitorrubim1@hotmail.com>
2019-07-23 22:04:03 -0300

    Implementado estrutura de listagem e classe de domínio endereço

---
+++ src/app/domain/Endereco.ts
@@ -0,0 +1,11 @@
+class endereco {
+    rua: String;
+    cep: String;
+    numero: String;
+    bairro: String;
+    cidade: String;
+    estado: String;
+    
+}
+
+export default endereco;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Endereco from 'src/app/domain/Endereco'
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,27 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  enderecos:Array<Object> = []
+
+  ionViewDidEnter() {
+    let endereco1 = new Endereco()
+    endereco1.rua = 'Rua Cambará'
+    endereco1.bairro = 'Jardim Miray'
+    endereco1.cidade = 'Itaquaquecetuba'
+    endereco1.estado = 'SP'
+    endereco1.numero = '866'
+
+    let endereco2 = new Endereco()
+    endereco2.rua = 'Rua Alvilândia'
+    endereco2.bairro = 'Jardim Caiuby'
+    endereco2.cidade = 'Itaquaquecetuba'
+    endereco2.estado = 'SP'
+    endereco2.numero = '235'
+
+
+    this.enderecos.push(endereco2)
+    this.enderecos.push(endereco1)
+  }
 
 }
